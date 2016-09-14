f9372dbbbb7795cf8bf01a69db97dd6f8bc39ae9
Ethan Favia <efavia215@live.com>
2016-09-14 12:58:34 -0400

    more work on server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/newsScrape');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.log('there was an error: ', err);
+});
+
+db.once('open', function(){
+
+    console.log('connection was successful to mongoDB')
+});
+
+var Note = require('./models/Note.js');
+
+var Article = require('./models/Article.js');
+
+
+app.get('/', function(req, res){
+    res.send(index.html);
+});
+
